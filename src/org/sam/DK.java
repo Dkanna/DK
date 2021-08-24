7af0a7b5faaf8e9ddc23d926ccff4f029925fb50
Dkanna <dineshkannasekar@gmail.com>
2021-08-24 11:57:48 +0530

    LoadUrl Done

--- src/org/sam/DK.java
+++ src/org/sam/DK.java
@@ -1,5 +1,13 @@
 package org.sam;
 
 public class DK {
+	
+	private static void LoadUrl() {
+		System.out.println("LoadUrl");
+	}
+	
+	public static void main(String[] args) {
+		LoadUrl();
+	}
 
 }
