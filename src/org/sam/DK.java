8aade5987fa4582ee36cc25092976f83d55a0830
Dkanna <dineshkannasekar@gmail.com>
2021-08-24 11:42:07 +0530

    GetDriver Loaded

--- src/org/sam/DK.java
+++ src/org/sam/DK.java
@@ -1,5 +1,13 @@
 package org.sam;
 
 public class DK {
+	
+	public static void LoadUrl() {
+		System.out.println("getDriver");
+	}
+	
+	public static void main(String[] args) {
+		LoadUrl();
+	}
 
 }
